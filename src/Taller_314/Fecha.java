5c584787354840c330be634b32daaa2374fe603a
Ethan Flores <ethan.sebas.los@gmail.com>
2025-10-18 14:07:23 -0500

    Crecion de la clase fecha

---
+++ src/Taller_314/Fecha.java
@@ -0,0 +1,47 @@
+package Taller_314;
+
+public class Fecha {
+    /**Creacion de los atributos de la clase*/
+    private int mes;
+    private int dia;
+    private int anio;
+
+    /**Constructor*/
+    public Fecha(int mes, int dia, int anio) {
+        this.mes = mes;
+        this.dia = dia;
+        this.anio = anio;
+    }
+
+    /**Metodos propios de Java*/
+    public int getMes() {
+        return mes;
+    }
+
+    public int getAnio() {
+        return anio;
+    }
+
+    public int getDia() {
+        return dia;
+    }
+
+    public void setMes(int mes) {
+        this.mes = mes;
+    }
+
+    public void setDia(int dia) {
+        this.dia = dia;
+    }
+
+    public void setAnio(int anio) {
+        this.anio = anio;
+    }
+
+    public String mostrarFecha(int dia, int mes,int anio){
+        String fecha = "La fecha es " + Integer.toString(dia)+ "/" + Integer.toString(mes)+"/"+Integer.toString(anio);
+        return fecha;
+    }
+}
+
+
---
+++ src/Taller_314/Main.java
@@ -0,0 +1,27 @@
+package Taller_314;
+
+import javax.swing.*;
+
+/**Cree una clase llamada Fecha,que incluya tres variables de instancia:
+ un mes (tipo int),
+ un día (tipo int)
+ y un año (tipo int).
+ Su clase debe tener un constructor que inicialice las tres variables de instancia,
+ y debe asumir que los valores que se proporcionan son correctos.
+ Proporcione un método establecer y un método obtener para cada variable de instancia.
+ Proporcione un método mostrarFecha, que muestre el mes, día y año, separados por barras diagonales (/).
+ Escriba una aplicación de prueba llamada PruebaFecha, que demuestre las capacidades de la clase Fecha.   */
+
+
+public class Main {
+    public static void main(String[] args) {
+        /**Creacion de objetos de la clase fecha*/
+        Fecha f1 = new Fecha(0,0,0);
+        /**Leer los valores de la fecha*/
+        f1.setDia(Integer.parseInt(JOptionPane.showInputDialog("Ingrese el dia")));
+        f1.setMes(Integer.parseInt(JOptionPane.showInputDialog("Ingrese el Mes")));
+        f1.setAnio(Integer.parseInt(JOptionPane.showInputDialog("Ingrese el año")));
+        /** LLamada al metodo y despliege*/
+        JOptionPane.showMessageDialog(null,f1.mostrarFecha(f1.getDia(),f1.getMes(),f1.getAnio()));
+    }
+}
